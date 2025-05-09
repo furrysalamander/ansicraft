7f82287981d8ecbd3948e130c418fade9567844f
Mike Abbott <furrysalamander@gmail.com>
2025-05-08 21:59:48 -0600

    attempting to disable rawMouseInput

--- entry-point.sh
+++ entry-point.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Start minecraft in the background using xvfb
-xvfb-run --listen-tcp --server-num 44 --auth-file /tmp/xvfb.auth -s "-ac -screen 0 1280x720x24 +extension XTEST" minecraft-launcher/start 1.18.2 docker >/dev/null 2>&1 &
+echo "rawMouseInput:false" > /root/minecraft-launcher/profiles/docker-base-1.18.2/options.txt
 
+xvfb-run --listen-tcp --server-num 44 --auth-file /tmp/xvfb.auth -s "-ac -screen 0 1280x720x24 +extension XTEST" minecraft-launcher/start 1.18.2 docker >/dev/null 2>&1 &
 # Wait for minecraft to start up
 sleep 30 # If your computer loads the world slower or faster, this can be adjusted.
 DISPLAY=:44 xdotool click 1
